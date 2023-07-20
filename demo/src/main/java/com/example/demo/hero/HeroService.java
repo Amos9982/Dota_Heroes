01e1dcc97b901f93fd85c177ba8d0cc070edd4cc
Amos9982 <amoswey@gmail.com>
2023-07-20 17:28:27 +0800

    Second Commit

--- demo/src/main/java/com/example/demo/hero/HeroService.java
+++ demo/src/main/java/com/example/demo/hero/HeroService.java
@@ -13,7 +13,7 @@ public class HeroService {
             Arrays.asList(
                     new Hero(1, "Night Stalker", "The beast of bedtime tales, Balanar is the primal terror that every child knows to fear.", "Void, Crippling Fear, Hunter in the Night, Dark Ascension"),
                     new Hero(2, "Earthshaker", "Woken from the depth of the earth, Raigor Stonehoof shapes the battlefield with seismic force.", "Fissue, Enchant Totem, Aftershock, Echo Slam"),
-                        new Hero(3, "Gyrocopter", "The world's first non-magical flying device took over a decade to create, and less than a few seconds to shoot down.", "Rocket Barrage, Homing Missle, Flak Cannon, Call Down"),
+                    new Hero(3, "Gyrocopter", "The world's first non-magical flying device took over a decade to create, and less than a few seconds to shoot down.", "Rocket Barrage, Homing Missle, Flak Cannon, Call Down"),
                     new Hero(4, "Legion Commander", "Renowned for her intrepid defense of Stonehall, the commander of the Bronze Legion swore a brutal vengeance upon its Abyssal Invaders.", "Overwhelming Odds, Press the Attack, Moment of Courage, Duel"),
                     new Hero(5, "Huskar", "Rescued against his will from the Nothl Realm, the revived Huskar found supernatural strength in his own living essence.", "Inner Fire, Burning Spear, Berserker's Blood, Life Break")
             )
